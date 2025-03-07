070ad23b7b63a990f2ff04d41e45f9262a9ccd58
Sohail Asif <sohailasuf123@gmail.com>
2025-03-07 21:36:32 +0500

    hi i am sohail

--- DAY-2/day2.js
+++ DAY-2/day2.js
@@ -19,3 +19,68 @@ console.log("devide  is",num1/num2);
 
 // -  Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console.
 console.log("Reminder is",num1%num2);
+
+
+
+// ## Activity 2: Assignment Operators
+
+// -  Task 6: Use the + operator to add a number to a variable and log the result to the console. 
+let c=num1+num2
+console.log(c);
+
+// -  Task 7: Use the operator to subtract a number from a variable and log the result to the console.
+let d=num1-num2
+console.log(d);
+
+
+
+// ## Activity 3: Comparison Operators
+
+// -  Task 8: Write a program to compare two numbers using > and < and log the result to the console.
+console.log(num1>num2);
+console.log(num1<num2);
+
+// -  Task 9: Write a program to compare two numbers using > and <= and log the result to the console. 
+console.log(num1>=num2);
+console.log(num1<=num2);
+
+
+// -  Task 10: Write a program to compare two numbers using == and === and log the result to the console.
+let num4=5
+let num5="5"
+console.log("num4==num5:",num4==num5);
+console.log("num4===num5:",num4===num5);
+
+
+
+
+
+
+
+// ## Activity 4: Logical Operators
+let age =17
+let eligible=false
+
+
+
+// -  Task 11: Write a program that uses the && operator to combine two conditions and log the result to the console.
+if (age>=18 && eligible) {
+    console.log("Allow For VOte");
+    
+}else{
+    console.log("dont allow");
+}
+
+// -  Task 12: Write a program that uses the || operator to combine two conditions and log the result to the console.
+if (age>=18 || eligible) {
+    console.log("Allow For VOte");
+    
+}else{
+    console.log("dont allow");
+}
+
+// -  Task 13: Write a program that uses the operator to negate a condition and log the result to the console.
+let isRaining = true;
+
+console.log("Before negation:", isRaining);
+console.log("After negation:", !isRaining);
