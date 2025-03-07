f4a698313f987d0d0b091c85106dd0794bff5756
Sohail Asif <sohailasuf123@gmail.com>
2025-03-07 19:55:18 +0500

    task 1 comp

---
+++ DAY-2/day2.js
@@ -0,0 +1,21 @@
+// ## Activity 1: Arithmetic Operations
+let num1 =4
+let num2=3
+
+// -  Task 1: Write a program to add two numbers and log the result to the console.
+console.log("sum is",num1+num2);
+
+// -  Task 2: Write a program to subtract two numbers and log the result to the console.
+console.log("subtract is",num1-num2);
+
+
+// -  Task 3: Write a program to multiply two numbers and log the result to the console.
+console.log("myltiply is",num1*num2);
+
+
+// -  Task 4: Write a program to divide two numbers and log the result to the console.
+console.log("devide  is",num1/num2);
+
+
+// -  Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console.
+console.log("Reminder is",num1%num2);
