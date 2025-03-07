51a717a5507649ecbb6b3ba950f6b3c6b10cf38c
Sohail Asif <sohailasuf123@gmail.com>
2025-03-07 17:38:13 +0500

    Task 3 done

---
+++ DAY-1/day1.js
@@ -0,0 +1,23 @@
+// Activity 1  Variable Declaration
+
+
+// -Task 1
+// Declare a variable using `var`, assign it a number, and log the value to the console.
+var num=6
+console.log(num);
+
+// Task 2:
+//  Declare a variable using `let, assign it a string, and log the value to the console.
+
+let name="sohail"
+console.log(name);
+
+
+// Activity 2 Constant Declearication
+
+// Task 3: Declare a variable using `const` , assign it a boolean value, and log the value to the console.
+const cons=true
+console.log(cons);
+
+
+
