8028bbc0799b6242433ed832653f98a4a2237b6e
Sohail Asif <sohailasuf123@gmail.com>
2025-03-07 17:43:54 +0500

    hi

--- DAY-1/day1.js
+++ DAY-1/day1.js
@@ -4,20 +4,41 @@
 // -Task 1
 // Declare a variable using `var`, assign it a number, and log the value to the console.
 var num=6
-console.log(num);
+// console.log(num);
 
 // Task 2:
 //  Declare a variable using `let, assign it a string, and log the value to the console.
 
 let name="sohail"
-console.log(name);
+// console.log(name);
 
 
 // Activity 2 Constant Declearication
 
 // Task 3: Declare a variable using `const` , assign it a boolean value, and log the value to the console.
 const cons=true
-console.log(cons);
+// console.log(cons);
+
+
+// ### Activity 3: Data Types
+// Task 4: Create variable of different data types (number, string, boolean, object, array) and log each variable's type using the `tyreof` operator.
+let number=4
+let city="kunri"
+let eligble=true
+let car={}
+let bike=[]
+// console.log(typeof(number));
+// console.log(typeof(city));
+// console.log(typeof(eligble));
+// console.log(typeof(car));
+// console.log(typeof(bike));
+// output
+// number
+// string
+// boolean
+// object
+// object
+
 
 
 
