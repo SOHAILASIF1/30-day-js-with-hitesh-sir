a5dc4c90064b4e5dab6bd295adbebef769e230db
Sohail Asif <sohailasuf123@gmail.com>
2025-03-09 15:35:53 +0500

    task 1

---
+++ DAY-3/day3.js
@@ -0,0 +1,16 @@
+// ## Activity 1: If-Else Statements
+
+// ### Task 1
+// - Write a program to check if a number is positive, negative, or zero, and log the result to the console.
+let num=-4
+
+if (num==0) {
+    console.log("number is zero");
+    
+}else if(num>0){
+    console.log("number is positive");
+
+} else if(num<0){
+    console.log("negetive");
+
+}
