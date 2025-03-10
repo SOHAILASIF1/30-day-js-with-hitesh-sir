ec3f456a03f085a768af84d665ba4a4e3659aeb5
Sohail Asif <sohailasuf123@gmail.com>
2025-03-10 11:22:21 +0500

    Day3

--- DAY-3/day3.js
+++ DAY-3/day3.js
@@ -5,12 +5,22 @@
 let num=-7
 
 if (num==0) {
-    console.log("number is zero");
+    // console.log("number is zero");
     
 }else if(num>0){
-    console.log("number is positive");
+    // console.log("number is positive");
 
 } else if(num<0){
     console.log("negetive");
 
+}
+
+// ### Task 2
+// - Write a program to check if a person is eligible to vote (age >= 18) and log the result to the console.
+let age=prompt("enter age")
+if (age >=18 ) {
+    console.log("You Are Eligible to vote");
+    
+}else{
+    console.log("You Are Not Eligible to VOTE");
 }
