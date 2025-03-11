d8e51a9b717cb1f602b8302fb3ce7f901cbf8c24
Sohail Asif <alyana998877@gmail.com>
2025-03-11 10:58:14 +0500

    sum ko 1

--- Day-4/day4.js
+++ Day-4/day4.js
@@ -16,7 +16,7 @@ for (let i = 1; i < 10 ; i++) {
 // ### Task 3
 // - Write a program to calculate the sum of numbers from 1 to 10 using a while loop.
 let num=1
-let sum=0
+let sum=1
 while (num<=10) {
     sum=sum+num
     num ++
