b223253bb0a93cebfd8775faf6f8cbfe18951150
SOHAILASIF1 <alyana998877@gmail.com>
2025-03-11 13:07:01 +0500

    tsk don

--- DAy-5/day5.js
+++ DAy-5/day5.js
@@ -48,3 +48,23 @@ const twoStr=function(str1,str2){
     return newStr
 }
 console.log(twoStr("ali","sohail"));
+
+
+// ## Activity 3: Arrow Functions
+
+// ### Task 5
+// Write an arrow function to calculate the sum of two numbers and return the result.
+const sumNum=(num1,num2)=>{
+    return num1+ num2
+
+}
+console.log(sumNum(2,5));
+
+// ### Task 6
+// Write an arrow function to check if a string contains a specific character and return a boolean value.
+const checkStr=(str)=>{
+   return str.includes("sohail")
+
+}
+
+console.log(checkStr("hello i am sohail"));
