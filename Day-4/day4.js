aeedb86fef627c0667e5751b0f373b7eb2fb69aa
Sohail Asif <sohailasuf123@gmail.com>
2025-03-11 10:50:50 +0500

    task 4 done

---
+++ Day-4/day4.js
@@ -0,0 +1,33 @@
+// ## Activity 1: For Loop
+
+// ### Task 1
+// - Write a program to print numbers from 1 to 10 using a for loop.
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+    
+}
+
+// ### Task 2
+// - Write a program to print the multiplication table of 5 using a for loop.
+for (let i = 1; i < 10 ; i++) {
+    console.log(5*i);
+    
+}
+// ### Task 3
+// - Write a program to calculate the sum of numbers from 1 to 10 using a while loop.
+let num=1
+let sum=0
+while (num<=10) {
+    sum=sum+num
+    num ++
+    
+}
+console.log(sum);
+// ### Task 4
+// - Write a program to print numbers from 10 to 1 using a while loop.
+let num1=10
+while (num1>=1) {
+    console.log(num1);
+    num1=num1-1
+    
+}
