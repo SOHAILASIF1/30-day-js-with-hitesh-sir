0f82a1b2f939c0e5cb46e4e59333b6a5cc00f150
SOHAILASIF1 <alyana998877@gmail.com>
2025-03-17 09:50:07 +0500

    ghjjkhvbj

--- DAY-9/day9.js
+++ DAY-9/day9.js
@@ -15,3 +15,45 @@ change.style.backgroundColor="red"
 let newDiv=document.createElement("div")
 newDiv.textContent="sohail Asif "
 document.body.append(newDiv)
+
+// ### Task 4
+// Create a new `li` element and add it to an existing `ul` list.
+let ul =document.getElementById("ul")
+let newLi=document.createElement('li')
+newLi.textContent="Sohail"
+ul.append(newLi)
+
+
+// ## Activity 3: Removing Elements
+
+// ### Task 5
+// Select an HTML element and remove it from the DOM.
+ul.remove()
+
+
+// ## Activity 4: Modifying Attributes and Classes
+
+// ### Task 7
+// Select an HTML element and change one of its attributes (e.g., `src` of an `img` tag).
+let element=document.getElementById('elem')
+element.setAttribute("src",'/jpg')
+
+// ### Task 8
+// Add and remove a CSS class to/from an HTML element.
+element.classList.add('new-class')
+element.classList.remove("new-class")
+
+// ## Activity 5: Event Handling
+
+// ### Task 9
+// Add a click event listener to a button that changes the text content of a paragraph.
+let para=document.getElementById('para1')
+let btn=document.getElementById('btn')
+btn.addEventListener('click',()=>{
+    para.textContent="sohail asif is a good"
+})
+
+// ### Task 10
+// Add a mouseover event listener to an element that changes its border color.
+
+
